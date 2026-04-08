53458cd973489182ae63d21b98b67943fb1d79fa
Musa-cyber666 <goodvbes7@gmail.com>
2026-04-09 00:21:27 +0700

    feat: day 1 - setup ml environment and folder structure.

---
+++ 01-Data-Preprocessing/environment_check.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import sklearn
+
+print("Pandas Version:", pd.__version__)
+print("NumPy Version:", np.__version__)
+print("Scikit-learn Version:", sklearn.__version__)
+print("ML Environment is Ready! Let's go, Zippy!")
